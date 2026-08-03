0f686fa131e769ec292fb015e6dcbf0b97502156
erickcsen <erick.pecinta.negeri@gmail.com>
2026-08-03 08:47:01 +0700

    React Route

---
+++ resources/js/pages/NotFound.jsx
@@ -0,0 +1,26 @@
+import Navbar from "./Layouts/NavbarMenu";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function NotFound() {
+    return (
+        <>
+            <Navbar />
+            <Container className="mt-5">
+                <Row className="justify-content-center">
+                    <Col md={6} lg={5} xl={4}>
+                        <div className="border rounded p-4 shadow-sm">
+                            <h3 className="text-center mb-4">
+                                404 Not Found
+                            </h3>
+                            <p className="text-center">
+                                The page you are looking for does not exist.
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
--- resources/js/pages/Page.jsx
+++ resources/js/pages/Page.jsx
@@ -4,6 +4,7 @@ import Welcome from "./Welcome";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Register from "./Register";
+import NotFound from "./NotFound";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Page() {
@@ -13,6 +14,7 @@ export default function Page() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     );

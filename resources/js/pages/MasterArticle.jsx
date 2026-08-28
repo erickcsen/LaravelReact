4d4a8f20be901344bbf87b80a2f39bd3d09949a6
erickcsen <erick.pecinta.negeri@gmail.com>
2026-08-28 15:29:28 +0700

    feat: enhance MasterArticle component for editing articles with improved loading and error handling

--- resources/js/pages/MasterArticle.jsx
+++ resources/js/pages/MasterArticle.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import NavBar from "./Layouts/NavbarMenu"
 import { Container, Row, Col, Button } from "react-bootstrap"
-import { Link, useParams, useNavigate } from "react-router-dom"
+import { Link, useParams, useNavigate, data } from "react-router-dom"
 import Form from 'react-bootstrap/Form'
 
 import api from "../api";
@@ -39,8 +39,12 @@ export default function MasterArticle() {
         user:(window.AppData.user)?window.AppData.user.id:"",
         image:[],
         category:"",
-        statusCategory:""
+        statusCategory:"",
+        defaultImg:""
     });
+
+    var [defaultImg, setDefaultImg] = useState("");
+
     const [errors, setErrors] = useState({
         title:"", category:"", content:"", statusCategory:"", image:""
     })
@@ -113,6 +117,10 @@ export default function MasterArticle() {
             .then((response) => {
                 console.log({dataWithID:response});
                 setDataArticle(response.data);
+                if(response.data)
+                    if(response.data)
+                        if(response.data.data.length > 0)
+                            setDefaultImg(domain+"/"+response.data.data[0].image_url);
             }).catch((errors)=>{
                 console.log(errors);
             }).finally(()=>{
@@ -226,7 +234,167 @@ export default function MasterArticle() {
             </ToastContainer>
         </>);
     }
-    else if (page=="article"){
+    else if (page=="edit"){
+        document.title = "Master Article - Edit Article";
+
+        if (loading) // Loading Dapatkan Data Article
+            return <>
+                <NavBar />
+                <Container>
+                    <Row>
+                        <Col>
+                            <sup><Link to="/master-articles" style={{textDecoration:"none"}}>Master Articles</Link> &gt; Article </sup> <br />
+                            <h1 className="text-center" style={{marginTop:"10vh"}}>
+                                <BlinkBlur color="#ffac00" size="large" text="Loading" textColor="" /> <br />
+                            </h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        else if(dataArticle.data.length > 0 == true) // Kalau ada data article
+            return (<>
+                <NavBar/>
+                <Container>
+                    <form onSubmit={handleSubmit}>
+                        <Row>
+                            {dataArticle.data.map((data)=>{
+                                form.title=data.title;
+                                form.category = data.category_id;
+                                form.defaultImg = domain+"/"+data.image_url;
+                                if (form.content=="") {
+                                    setContent(data.description);
+                                    form.content = data.description;
+                                }
+                            })}
+                            <Col>
+                                <sup><Link to="/master-articles" style={{textDecoration:"none"}}>Master Articles</Link> &gt;
+                                <Link to={"/master-articles/article/"+id} style={{textDecoration:"none"}}>{form.title}</Link> &gt;
+                                Edit Article </sup> <br />
+                                <b className="h5">
+                                    Edit Article
+                                </b>
+                            </Col>
+                            <Col className="d-none d-md-block">
+                                <button type="submit" className="btn btn-primary float-end">Save Article</button>
+                            </Col>
+                            <div className="position-fixed end-0 start-0 bottom-0 border py-2 bg-white d-block d-md-none" style={{zIndex:"1000"}}>
+                                <button type="submit" className="btn btn-primary">Save Article</button>
+                                <Link to="/master-articles" className="btn btn-danger ms-3">Cancel</Link>
+                            </div>
+                        </Row>
+                        <Row>
+                            <Col xs={12} md={6} lg={5} xl={4}>
+                                <div
+                                    style={{
+                                        height: "90%",
+                                        display: "flex",
+                                        alignItems: "center",
+                                        justifyContent: "center"
+                                    }}
+                                >
+                                    <div style={{textAlign:"center"}}>
+                                        <img
+                                            src={defaultImg}
+                                            width="100%"
+                                            className="rounded"
+                                            alt=""
+                                        /> <br/>
+                                        <label className="btn btn-light mt-3" htmlFor="banner">
+                                            Change Image
+                                        </label>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col className="pt-3" xs={12} md={6} lg={7} xl={8}>
+                                <input type="hidden" value={csrfToken}/>
+                                <Row>
+                                    <Col xs="12" md="8" lg="9">
+                                        <label>Title</label>
+                                        <div className="input-group">
+                                            <Form.Control type="text" placeholder="Input title" isInvalid={!!errors.title} value={form.title} onChange={(e) => {setForm({...form, title: e.target.value}); setErrors({...errors, title:""})}}/>
+                                        </div>
+                                        {errors.title && <div className="text-danger">{errors.title[0]}</div>}
+                                    </Col>
+                                    <Col xs="12" md="4" lg="3">
+                                        <label>Category</label>
+                                        <div className="input-group">
+                                            <Form.Select onChange={(e) => {setForm({...form, category: e.target.value, statusCategory: (e.target.value=="New")?e.target.value:""});setErrors({...errors, category:""})}} isInvalid={!!errors.category} className={(form.statusCategory=="New")?"d-none":"rounded"} style={{textTransform:"capitalize"}} value={form.category}>
+                                                <option key={0} value="">-- Select Category --</option>
+                                                <option key={1} value="New">-- New Category --</option>
+                                                {category_list.map((item, index)=>{
+                                                    return <option key={index+1} value={item.id} style={{textTransform:"capitalize"}}>{item.title}</option>
+                                                })}
+                                            </Form.Select>
+                                            <input onChange={(e) => {setForm({...form, category: e.target.value});setErrors({...errors, category:""})}} className={(form.statusCategory=="New")?"form-control rounded":"form-control d-none"} placeholder="Input new Category"/>
+                                        </div>
+                                        {errors.category && <div className="text-danger">{errors.category[0]}</div>}
+                                    </Col>
+                                    <Col xs="12" className="mt-1 d-none">
+                                        <label>Banner</label>
+                                        <div className="input-group">
+                                            <Form.Control id="banner" type="file" accept="image/*" onChange={(e)=>{setForm({...form, image: (e.target.files.length > 0)?e.target.files[0]:e.target.value}); setDefaultImg(URL.createObjectURL(e.target.files[0])); setErrors({...errors, image:""})}} isInvalid={!!errors.image} />
+                                        </div>
+                                        {errors.image && <div className="text-danger">{errors.image[0]}</div>}
+                                    </Col>
+                                    <Col className={!errors.content ? "mt-3" : "mt-1"} style={{paddingBottom:"66px"}}>
+                                        {errors.content && <div className="text-danger">{errors.content[0]}</div>}
+                                        <Col className="p-0" style={(errors.content)?{border:"solid 1px red"}:{}}>
+                                            <CKEditor
+                                                editor={ClassicEditor}
+                                                data={content}
+                                                onChange={(event, editor) => {
+                                                    const data = editor.getData();
+                                                    setForm({...form, content: data})
+                                                    setErrors({...errors, content:""})
+                                                }}
+                                            />
+                                        </Col>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </form>
+                </Container>
+                <ToastContainer position="top-end" className="p-3">
+                    <Toast
+                        onClose={() => setShowToast(false)}
+                        show={showToast}
+                        delay={10000}
+                        autohide
+                        bg={errors.message ? "danger" : "success"}
+                    >
+                        <Toast.Header>
+                            <strong className="me-auto">{errors.message ? "Insert Articles Failed" : "Info"}</strong>
+                        </Toast.Header>
+
+                        <Toast.Body className="text-white">
+                            {errors.message ? errors.message : "Insert Articles Successful."}
+                        </Toast.Body>
+                    </Toast>
+                </ToastContainer>
+            </>);
+        else if(id != null && dataArticle.data.length > 0 == false) return <>
+            <NavBar/>
+            <Container>
+                <Row>
+                    <Col>
+                        <sup><Link to="/master-articles" style={{textDecoration:"none"}}>Master Articles</Link> &gt; Article </sup> <br />
+                        <Row>
+                            <p className="text-center" style={{marginTop:"10vh",fontSize:"14pt"}}>
+                                <i className="fa fa-filter" style={{fontSize:"100px"}}></i><br/>
+                                <b>
+                                    Tidak Ada Data
+                                </b> <br />
+                                <Link to="/master-articles">
+                                    <Button className="btn btn-light border mt-3">Back</Button>
+                                </Link>
+                            </p>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    }else if (page=="article"){
         document.title = "Master Article - Read Article";
         if (id==null) navigate("/master-articles");
 
@@ -255,7 +423,6 @@ export default function MasterArticle() {
                                 <Row>
                                     <Col xs={12} md={5} lg={4} xl={3}>
                                         <div
-                                            className="rounded"
                                             style={{
                                                 height: "100%",
                                                 display: "flex",

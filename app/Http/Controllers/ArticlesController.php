8e4ec355d3fc119e65e45a26bc8cdb58be1fd174
erickcsen <erick.pecinta.negeri@gmail.com>
2026-08-28 10:19:19 +0700

    feat: update pagination limit to 10 for listArticleWithAuthentication method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -101,7 +101,7 @@ class ArticlesController extends Controller
         $data = [];
         $userID = Auth::user()->id;
 
-        $data = Articles::where(["user_id"=>$userID])->paginate(1);
+        $data = Articles::where(["user_id"=>$userID])->paginate(10);
 
         return response()->json($data, 200);
     }

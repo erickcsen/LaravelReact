efe043f3424b2160ac550cce4f4cbd048547cf07
erickcsen <erick.pecinta.negeri@gmail.com>
2026-08-31 10:56:11 +0700

    feat: implement loading states and structure for MasterArticle component

--- resources/js/pages/MasterArticle.jsx
+++ resources/js/pages/MasterArticle.jsx
@@ -1,6 +1,82 @@
 import NavBar from "./Layouts/NavbarMenu"
+
+const Master = {article:null, create:null, update:null, list:null, loading:{index:null, article:null}}
+
+const initialForm = {
+    title: "",
+    content: "",
+    user: window.AppData?.user?.id || "",
+    image: null,
+    category: "",
+    statusCategory: "",
+};
+
+const initialErrors = {
+    title: "",
+    category: "",
+    content: "",
+    statusCategory: "",
+    image: "",
+    message: "",
+};
+
+Master.loading.index = () => <>
+    <NavBar />
+    <Container>
+        <Row>
+            <Col>
+                <b className="h5">
+                    Master Article
+                </b>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Link to="/master-articles/create" className="btn btn-primary mt-3">New Article</Link>
+            </Col>
+            <h1 className="text-center" style={{marginTop:"10vh"}}>
+                <BlinkBlur color="#ffac00" size="large" text="Loading" textColor="" /> <br />
+            </h1>
+        </Row>
+    </Container>
+</>
+
+Master.loading.article = () => <>
+    <NavBar />
+    <Container>
+        <Row>
+            <Col>
+                <sup><Link to="/master-articles" style={{textDecoration:"none"}}>Master Articles</Link> &gt; Article </sup> <br />
+                <h1 className="text-center" style={{marginTop:"10vh"}}>
+                    <BlinkBlur color="#ffac00" size="large" text="Loading" textColor="" /> <br />
+                </h1>
+            </Col>
+        </Row>
+    </Container>
+</>
+
+Master.list = ()=>{
+    return ;
+}
+
+Master.article = ()=>{
+    return ;
+}
+
+Master.create = ()=>{
+    return ;
+}
+
+Master.update = ()=>{
+    return ;
+}
+
 export default function MasterArticle() {
-    return <>
-        <NavBar/>
-    </>
+    const { page, parameter_id } = useParams();
+    const navigate = useNavigate();
+
+    return (page == "create")? Master.create
+        : (page == "edit") ? Master.update
+        : (page == "article") ? Master.article
+        : Master.list;
 }
